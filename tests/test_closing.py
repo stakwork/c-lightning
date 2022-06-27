23d8a8763c236103e07fa0321bdfd0254cc77005
Ken Sedgwick <ken@bonsai.com>
2022-06-27 16:46:56 -0700

    Skip tests which use dev_sign_last_tx to cause breach

--- tests/test_closing.py
+++ tests/test_closing.py
@@ -484,6 +484,7 @@ def test_closing_negotiation_step_700sat(node_factory, bitcoind, chainparams):
     closing_negotiation_step(node_factory, bitcoind, chainparams, opts)
 
 
+@unittest.skipIf(os.getenv('SUBDAEMON').startswith('hsmd:remote_hsmd'), "dev_sign_last_tx causes subsequent validate_holder_commitment_tx failure")
 @pytest.mark.developer("needs dev-disable-commit-after")
 @pytest.mark.parametrize("anchors", [False, True])
 def test_penalty_inhtlc(node_factory, bitcoind, executor, chainparams, anchors):
@@ -616,6 +617,7 @@ def test_penalty_inhtlc(node_factory, bitcoind, executor, chainparams, anchors):
     check_utxos_channel(l2, [channel_id], expected_2, tags)
 
 
+@unittest.skipIf(os.getenv('SUBDAEMON').startswith('hsmd:remote_hsmd'), "dev_sign_last_tx causes subsequent validate_holder_commitment_tx failure")
 @pytest.mark.developer("needs dev-disable-commit-after")
 @pytest.mark.parametrize("anchors", [False, True])
 def test_penalty_outhtlc(node_factory, bitcoind, executor, chainparams, anchors):
